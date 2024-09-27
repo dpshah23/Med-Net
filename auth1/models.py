981230bd45d74e7a63865f5c1e63b053058549d9
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-09-27 23:11:10 +0530

    Added Login

--- auth1/models.py
+++ auth1/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your models here.
+class User(models.Model):
+    id=models.CharField(max_length=100,primary_key=True)
+    email=models.EmailField()
+    password=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
