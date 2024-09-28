8b542414061a58eed90c4a9599aa8d1300808abd
Nakshi Shah <nakshishah3108@gmail.com>
2024-09-28 15:29:21 +0530

    appointments

--- home/models.py
+++ home/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 import datetime
-import django
-import django.utils
 import django.utils.timezone
 
 # Create your models here.
@@ -10,7 +8,21 @@ class ContactUs(models.Model):
     email=models.EmailField(max_length=100)
     mobile=models.CharField(max_length=15)
     message=models.TextField()
-    date=models.DateField(default=django.utils.timezone.now())
+    date=models.DateField()
 
     def __str__(self):
         return self.name
+    
+class appointments(models.Model):
+    doc_email = models.CharField(max_length=200)
+    p_email = models.CharField(max_length=200 , default=None)
+    date = models.DateField()
+    time = models.TimeField()
+    specialization = models.CharField(max_length=100)
+    payment = models.CharField(max_length=10)
+    vc_link = models.TextField(default=None)
+    completed = models.BooleanField(default=False)
+    doc_paid = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return "{doc_email} and {p_email}"
