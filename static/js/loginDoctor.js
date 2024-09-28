27448a1295e5b5ceaf36dceed35f9c8cdfadb034
Kukshi <85385763+Shah-Kukshi@users.noreply.github.com>
2024-09-28 15:53:58 +0530

    Doctor Login Page

--- static/js/loginDoctor.js
+++ static/js/loginDoctor.js
@@ -1,7 +1,10 @@
+
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const container = document.querySelector(".container");
 const loginContainer = document.querySelector(".login-container"); // Select the login container
+
+
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -15,6 +18,7 @@ sign_up_btn.addEventListener("click", () => {
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+  
 
   // Delay hiding the login container by 1 second (1000 milliseconds)
   setTimeout(() => {
@@ -49,15 +53,39 @@ togglePassword("#toggle-signup-password", "#signup-password");
  let login = document.querySelector(".login-login");
  let slider = document.querySelector(".login-slider");
  let formSection = document.querySelector(".login-form-section");
+ let change = document.querySelector(".change-color")
 
  signup.addEventListener("click", () => {
      slider.classList.add("login-moveslider");
      formSection.classList.add("login-form-section-move");
+     change.classList.add("change-color")
+
+   
+
  });
 
  login.addEventListener("click", () => {
      slider.classList.remove("login-moveslider");
      formSection.classList.remove("login-form-section-move");
+
  });
 
- 
+
+ function togglePasswordVisibility(inputId) {
+    const inputField = document.getElementById(inputId);
+    inputField.type = inputField.type === 'password' ? 'text' : 'password';
+}
+
+function checkPasswords() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    const errorElement = document.getElementById('password-error');
+
+    if (password !== confirmPassword) {
+        errorElement.style.display = 'block';
+    } else {
+        errorElement.style.display = 'none';
+        // Submit the form or perform further actions
+        alert('Form submitted successfully!');
+    }
+}
